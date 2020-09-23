da9aa14be45dffadacb41e3ebc36ac8059cbc2cf
Jon Cram <webignition@gmail.com>
2020-09-23 16:30:42 +0100

    Fix CS

--- tests/Functional/Entity/JobStateTest.php
+++ tests/Functional/Entity/JobStateTest.php
@@ -30,12 +30,12 @@ class JobStateTest extends AbstractEntityTest
         $state1 = JobState::create($name);
         $state2 = JobState::create($name);
 
+        $this->expectException(UniqueConstraintViolationException::class);
+        $this->expectExceptionMessage('Key (name)=(job-state-name) already exists');
+
         if ($this->entityManager instanceof EntityManagerInterface) {
             $this->entityManager->persist($state1);
             $this->entityManager->persist($state2);
-
-            $this->expectException(UniqueConstraintViolationException::class);
-            $this->expectExceptionMessage('Key (name)=(job-state-name) already exists');
 
             $this->entityManager->flush();
         }
