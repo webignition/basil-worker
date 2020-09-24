636aa53e80f711e6c71ba219049f448c29370e3b
Jon Cram <webignition@gmail.com>
2020-09-24 11:22:23 +0100

    Factor out entity state commonalities

--- src/Entity/JobState.php
+++ src/Entity/JobState.php
@@ -7,35 +7,13 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * @ORM\Entity
  */
-class JobState
+class JobState extends AbstractState
 {
-    /**
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="IDENTITY")
-     * @ORM\Column(type="integer")
-     */
-    private ?int $id = null;
-
-    /**
-     * @ORM\Column(type="string", length=255, nullable=false, unique=true)
-     */
-    private string $name;
-
     public static function create(string $name): self
     {
         $state = new JobState();
         $state->name = $name;
 
         return $state;
-    }
-
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
-
-    public function getName(): ?string
-    {
-        return $this->name;
     }
 }
