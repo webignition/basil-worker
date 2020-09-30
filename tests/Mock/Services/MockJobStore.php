a4d1ae4cc785cf04aa499a3f97d0e04702ed81a3
Jon Cram <webignition@gmail.com>
2020-09-30 16:25:39 +0100

    Add job as property of JobStore

---
+++ tests/Mock/Services/MockJobStore.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Mock\Services;
+
+use App\Entity\Job;
+use App\Services\JobStore;
+
+class MockJobStore
+{
+    private JobStore $jobStore;
+
+    public function __construct()
+    {
+        $this->jobStore = \Mockery::mock(JobStore::class);
+    }
+
+    public function getMock(): JobStore
+    {
+        return $this->jobStore;
+    }
+
+    public function withHasJobCall(bool $return): self
+    {
+        $this->jobStore
+            ->shouldReceive('hasJob')
+            ->andReturn($return);
+
+        return $this;
+    }
+
+    public function withCreateCall(string $label, string $callbackUrl): self
+    {
+        $this->jobStore
+            ->shouldReceive('create')
+            ->with($label, $callbackUrl);
+
+        return $this;
+    }
+
+    public function withGetJobCall(Job $job): self
+    {
+        $this->jobStore
+            ->shouldReceive('getJob')
+            ->andReturn($job);
+
+        return $this;
+    }
+}
