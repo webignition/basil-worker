5a738327c97686b4d945c42a0c7c560319ba1083
Jon Cram <webignition@gmail.com>
2020-11-10 11:51:21 +0000

    Create SendCallbackMessageDispatcher (#376)

--- src/EventSubscriber/TestExecuteDocumentReceivedEventSubscriber.php
+++ src/EventSubscriber/TestExecuteDocumentReceivedEventSubscriber.php
@@ -6,23 +6,16 @@ namespace App\EventSubscriber;
 
 use App\Event\TestExecuteDocumentReceivedEvent;
 use App\Event\TestFailedEvent;
-use App\Message\SendCallback;
-use App\Model\Callback\ExecuteDocumentReceived;
 use App\Model\Document\Step;
 use Psr\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\Messenger\MessageBusInterface;
 
 class TestExecuteDocumentReceivedEventSubscriber implements EventSubscriberInterface
 {
-    private MessageBusInterface $messageBus;
     private EventDispatcherInterface $eventDispatcher;
 
-    public function __construct(
-        MessageBusInterface $messageBus,
-        EventDispatcherInterface $eventDispatcher
-    ) {
-        $this->messageBus = $messageBus;
+    public function __construct(EventDispatcherInterface $eventDispatcher)
+    {
         $this->eventDispatcher = $eventDispatcher;
     }
 
@@ -31,32 +24,17 @@ class TestExecuteDocumentReceivedEventSubscriber implements EventSubscriberInter
         return [
             TestExecuteDocumentReceivedEvent::class => [
                 ['dispatchTestFailedEventIfStepFailed', 10],
-                ['dispatchSendCallbackMessage', 0],
             ],
         ];
     }
 
     public function dispatchTestFailedEventIfStepFailed(TestExecuteDocumentReceivedEvent $event): void
     {
-        $this->executeIfStepFailed($event, function (TestExecuteDocumentReceivedEvent $event) {
-            $this->eventDispatcher->dispatch(new TestFailedEvent($event->getTest()));
-        });
-    }
-
-    public function dispatchSendCallbackMessage(TestExecuteDocumentReceivedEvent $event): void
-    {
-        $message = new SendCallback(new ExecuteDocumentReceived($event->getDocument()));
-
-        $this->messageBus->dispatch($message);
-    }
-
-    private function executeIfStepFailed(TestExecuteDocumentReceivedEvent $event, callable $callback): void
-    {
         $document = $event->getDocument();
 
         $step = new Step($document);
         if ($step->isStep() && $step->statusIsFailed()) {
-            $callback($event);
+            $this->eventDispatcher->dispatch(new TestFailedEvent($event->getTest()));
         }
     }
 }
