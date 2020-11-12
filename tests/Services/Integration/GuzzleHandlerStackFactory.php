0d1c532fc87f4988cc1735d6d7240cde9a0de5de
Jon Cram <webignition@gmail.com>
2020-11-12 15:04:45 +0000

    Be more generic about GuzzleHandlerStackFactory::create() args

--- tests/Services/Integration/GuzzleHandlerStackFactory.php
+++ tests/Services/Integration/GuzzleHandlerStackFactory.php
@@ -6,12 +6,11 @@ namespace App\Tests\Services\Integration;
 
 use GuzzleHttp\HandlerStack;
 use GuzzleHttp\Middleware;
-use TijmenWierenga\Guzzle\Mocking\MockHandler;
 use webignition\HttpHistoryContainer\LoggableContainer;
 
 class GuzzleHandlerStackFactory
 {
-    public function create(MockHandler $handler, LoggableContainer $historyContainer): HandlerStack
+    public function create(callable $handler, LoggableContainer $historyContainer): HandlerStack
     {
         $handlerStack = HandlerStack::create($handler);
         $handlerStack->push(
