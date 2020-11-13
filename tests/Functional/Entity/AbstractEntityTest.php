8b3471a8d8b72d84bda23d4178dacae7ed766809
Jon Cram <webignition@gmail.com>
2020-11-13 10:21:58 +0000

    Use trait in entity tests

--- tests/Functional/Entity/AbstractEntityTest.php
+++ tests/Functional/Entity/AbstractEntityTest.php
@@ -5,21 +5,18 @@ declare(strict_types=1);
 namespace App\Tests\Functional\Entity;
 
 use App\Tests\AbstractBaseFunctionalTest;
+use App\Tests\Functional\TestClassServicePropertyInjectorTrait;
 use Doctrine\ORM\EntityManagerInterface;
 
 abstract class AbstractEntityTest extends AbstractBaseFunctionalTest
 {
+    use TestClassServicePropertyInjectorTrait;
+
     protected EntityManagerInterface $entityManager;
 
     protected function setUp(): void
     {
         parent::setUp();
-
-        $entityManager = self::$container->get(EntityManagerInterface::class);
-        self::assertInstanceOf(EntityManagerInterface::class, $entityManager);
-
-        if ($entityManager instanceof EntityManagerInterface) {
-            $this->entityManager = $entityManager;
-        }
+        $this->injectContainerServicesIntoClassProperties();
     }
 }
