4b8498f2297662a6528964a03ed243869a15af66
Jon Cram <webignition@gmail.com>
2020-11-13 16:58:23 +0000

    Create CallbackRepository (#426)

---
+++ src/Repository/CallbackRepository.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\CallbackEntity;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method CallbackEntity|null find($id, $lockMode = null, $lockVersion = null)
+ * @method CallbackEntity|null findOneBy(array $criteria, array $orderBy = null)
+ * @method CallbackEntity[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ *
+ * @extends ServiceEntityRepository<CallbackEntity>
+ */
+class CallbackRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, CallbackEntity::class);
+    }
+}
