f0cb4b378d9ff4b5a5ee5a7f24c3742283254e09
Jon Cram <webignition@gmail.com>
2020-11-17 12:05:55 +0000

    Add CallbackRepository::getFinishedCount()

--- tests/Functional/Repository/CallbackRepositoryTest.php
+++ tests/Functional/Repository/CallbackRepositoryTest.php
@@ -34,4 +34,70 @@ class CallbackRepositoryTest extends AbstractBaseFunctionalTest
         $retrievedCallback = $this->repository->find($callback->getId());
         self::assertEquals($callback, $retrievedCallback);
     }
+
+    /**
+     * @dataProvider getFinishedCountDataProvider
+     *
+     * @param array<CallbackInterface::STATE_*> $callbackStates
+     * @param int $expectedFinishedCount
+     */
+    public function testGetFinishedCount(array $callbackStates, int $expectedFinishedCount)
+    {
+        foreach ($callbackStates as $callbackState) {
+            $callback = CallbackEntity::create(CallbackEntity::TYPE_COMPILE_FAILURE, []);
+            $callback->setState($callbackState);
+            $this->store->store($callback);
+        }
+
+        self::assertSame($expectedFinishedCount, $this->repository->getFinishedCount());
+    }
+
+    public function getFinishedCountDataProvider(): array
+    {
+        return [
+            'no callbacks' => [
+                'callbackStates' => [],
+                'expectedFinishedCount' => 0,
+            ],
+            'none finished' => [
+                'callbackStates' => [
+                    CallbackInterface::STATE_AWAITING,
+                    CallbackInterface::STATE_QUEUED,
+                    CallbackInterface::STATE_SENDING,
+                ],
+                'expectedFinishedCount' => 0,
+            ],
+            'one complete' => [
+                'callbackStates' => [
+                    CallbackInterface::STATE_AWAITING,
+                    CallbackInterface::STATE_QUEUED,
+                    CallbackInterface::STATE_SENDING,
+                    CallbackInterface::STATE_COMPLETE,
+                ],
+                'expectedFinishedCount' => 1,
+            ],
+            'one failed' => [
+                'callbackStates' => [
+                    CallbackInterface::STATE_AWAITING,
+                    CallbackInterface::STATE_QUEUED,
+                    CallbackInterface::STATE_SENDING,
+                    CallbackInterface::STATE_FAILED,
+                ],
+                'expectedFinishedCount' => 1,
+            ],
+            'two complete, three failed' => [
+                'callbackStates' => [
+                    CallbackInterface::STATE_AWAITING,
+                    CallbackInterface::STATE_QUEUED,
+                    CallbackInterface::STATE_SENDING,
+                    CallbackInterface::STATE_COMPLETE,
+                    CallbackInterface::STATE_COMPLETE,
+                    CallbackInterface::STATE_FAILED,
+                    CallbackInterface::STATE_FAILED,
+                    CallbackInterface::STATE_FAILED,
+                ],
+                'expectedFinishedCount' => 5,
+            ],
+        ];
+    }
 }
