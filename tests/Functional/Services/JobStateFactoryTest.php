c36323dffb685f2ce45b7f5c72126ac4ea4296c6
Jon Cram <webignition@gmail.com>
2020-11-20 17:45:53 +0000

    Use invokables in JobStateFactoryTest

--- tests/Functional/Services/JobStateFactoryTest.php
+++ tests/Functional/Services/JobStateFactoryTest.php
@@ -4,19 +4,21 @@ declare(strict_types=1);
 
 namespace App\Tests\Functional\Services;
 
-use App\Entity\Callback\CompileFailureCallback;
+use App\Entity\Callback\CallbackInterface;
 use App\Entity\Test;
 use App\Model\JobState;
-use App\Services\CallbackStore;
 use App\Services\JobStateFactory;
-use App\Services\JobStore;
-use App\Services\TestFactory;
-use App\Services\TestStore;
 use App\Tests\AbstractBaseFunctionalTest;
-use App\Tests\Mock\MockSuiteManifest;
-use webignition\BasilCompilerModels\ErrorOutputInterface;
-use webignition\BasilCompilerModels\TestManifest;
-use webignition\BasilModels\Test\Configuration;
+use App\Tests\Model\EndToEndJob\Invokable;
+use App\Tests\Model\EndToEndJob\InvokableCollection;
+use App\Tests\Model\EndToEndJob\InvokableInterface;
+use App\Tests\Services\InvokableFactory\CallbackSetup;
+use App\Tests\Services\InvokableFactory\CallbackSetupInvokableFactory;
+use App\Tests\Services\InvokableFactory\JobSetup;
+use App\Tests\Services\InvokableFactory\JobSetupInvokableFactory;
+use App\Tests\Services\InvokableFactory\TestSetup;
+use App\Tests\Services\InvokableFactory\TestSetupInvokableFactory;
+use App\Tests\Services\InvokableHandler;
 use webignition\SymfonyTestServiceInjectorTrait\TestClassServicePropertyInjectorTrait;
 
 class JobStateFactoryTest extends AbstractBaseFunctionalTest
@@ -24,6 +26,7 @@ class JobStateFactoryTest extends AbstractBaseFunctionalTest
     use TestClassServicePropertyInjectorTrait;
 
     private JobStateFactory $jobStateFactory;
+    private InvokableHandler $invokableHandler;
 
     protected function setUp(): void
     {
@@ -34,14 +37,9 @@ class JobStateFactoryTest extends AbstractBaseFunctionalTest
     /**
      * @dataProvider createDataProvider
      */
-    public function testCreate(callable $setup, JobState $expectedState)
+    public function testCreate(InvokableInterface $setup, JobState $expectedState)
     {
-        $setup([
-            JobStore::class => self::$container->get(JobStore::class),
-            CallbackStore::class => self::$container->get(CallbackStore::class),
-            TestFactory::class => self::$container->get(TestFactory::class),
-            TestStore::class => self::$container->get(TestStore::class),
-        ]);
+        $this->invokableHandler->invoke($setup);
 
         self::assertEquals($expectedState, $this->jobStateFactory->create());
     }
@@ -50,251 +48,119 @@ class JobStateFactoryTest extends AbstractBaseFunctionalTest
     {
         return [
             'compilation-awaiting: no job' => [
-                'setup' => function () {
-                },
+                'setup' => Invokable::createEmpty(),
                 'expectedState' => new JobState(JobState::STATE_COMPILATION_AWAITING),
             ],
             'compilation-awaiting: has job, no sources' => [
-                'setup' => function (array $services) {
-                    $jobStore = $services[JobStore::class];
-                    if ($jobStore instanceof JobStore) {
-                        $jobStore->create('', '');
-                    }
-                },
+                'setup' => JobSetupInvokableFactory::setup(),
                 'expectedState' => new JobState(JobState::STATE_COMPILATION_AWAITING),
             ],
             'compilation-running: has job, has sources, no sources compiled' => [
-                'setup' => function (array $services) {
-                    $jobStore = $services[JobStore::class];
-                    if ($jobStore instanceof JobStore) {
-                        $job = $jobStore->create('', '');
-                        $job->setSources([
+                'setup' => JobSetupInvokableFactory::setup(
+                    (new JobSetup())
+                        ->withSources([
                             'Test/test1.yml',
                             'Test/test2.yml',
-                        ]);
-                        $jobStore->store($job);
-                    }
-                },
+                        ])
+                ),
                 'expectedState' => new JobState(JobState::STATE_COMPILATION_RUNNING),
             ],
             'compilation-failed: has job, has sources, has more than zero compile-failure callbacks' => [
-                'setup' => function (array $services) {
-                    $jobStore = $services[JobStore::class];
-                    if ($jobStore instanceof JobStore) {
-                        $job = $jobStore->create('', '');
-                        $job->setSources([
-                            'Test/test1.yml',
-                            'Test/test2.yml',
-                        ]);
-                        $jobStore->store($job);
-                    }
-
-                    $errorOutput = \Mockery::mock(ErrorOutputInterface::class);
-                    $errorOutput
-                        ->shouldReceive('getData')
-                        ->andReturn([]);
-
-                    $compileFailureCallback = new CompileFailureCallback($errorOutput);
-
-                    $callbackStore = $services[CallbackStore::class];
-                    if ($callbackStore instanceof CallbackStore) {
-                        $callbackStore->store($compileFailureCallback);
-                    }
-                },
+                'setup' => new InvokableCollection([
+                    JobSetupInvokableFactory::setup(
+                        (new JobSetup())
+                            ->withSources([
+                                'Test/test1.yml',
+                                'Test/test2.yml',
+                            ])
+                    ),
+                    CallbackSetupInvokableFactory::setup(
+                        (new CallbackSetup())
+                            ->withType(CallbackInterface::TYPE_COMPILE_FAILURE),
+                    )
+                ]),
                 'expectedState' => new JobState(JobState::STATE_COMPILATION_FAILED),
             ],
             'execution-awaiting: compilation workflow complete and execution workflow not started' => [
-                'setup' => function (array $services) {
-                    $jobStore = $services[JobStore::class];
-                    if ($jobStore instanceof JobStore) {
-                        $job = $jobStore->create('', '');
-                        $job->setSources([
-                            'Test/test1.yml',
-                        ]);
-                        $jobStore->store($job);
-                    }
-
-                    $testManifests = [
-                        new TestManifest(
-                            new Configuration('chrome', 'http://example.com'),
-                            '/app/source/Test/test1.yml',
-                            '/app/tests/GeneratedTest1.php',
-                            1
-                        ),
-                    ];
-
-                    $manifest = (new MockSuiteManifest())
-                        ->withGetTestManifestsCall($testManifests)
-                        ->getMock();
-
-                    $testFactory = $services[TestFactory::class];
-                    if ($testFactory instanceof TestFactory) {
-                        $testFactory->createFromManifestCollection($manifest->getTestManifests());
-                    }
-                },
+                'setup' => new InvokableCollection([
+                    JobSetupInvokableFactory::setup(
+                        (new JobSetup())
+                            ->withSources([
+                                'Test/test1.yml',
+                            ])
+                    ),
+                    TestSetupInvokableFactory::setup(
+                        (new TestSetup())
+                            ->withSource('/app/source/Test/test1.yml'),
+                    )
+                ]),
                 'expectedState' => new JobState(JobState::STATE_EXECUTION_AWAITING),
             ],
             'execution-running' => [
-                'setup' => function (array $services) {
-                    $jobStore = $services[JobStore::class];
-                    if ($jobStore instanceof JobStore) {
-                        $job = $jobStore->create('', '');
-                        $job->setSources([
-                            'Test/test1.yml',
-                            'Test/test2.yml',
-                        ]);
-                        $jobStore->store($job);
-                    }
-
-                    $testManifests = [
-                        new TestManifest(
-                            new Configuration('chrome', 'http://example.com'),
-                            '/app/source/Test/test1.yml',
-                            '/app/tests/GeneratedTest1.php',
-                            1
-                        ),
-                        new TestManifest(
-                            new Configuration('chrome', 'http://example.com'),
-                            '/app/source/Test/test2.yml',
-                            '/app/tests/GeneratedTest2.php',
-                            1
-                        ),
-                    ];
-
-                    $manifest = (new MockSuiteManifest())
-                        ->withGetTestManifestsCall($testManifests)
-                        ->getMock();
-
-                    $testFactory = $services[TestFactory::class];
-                    if ($testFactory instanceof TestFactory) {
-                        $tests = $testFactory->createFromManifestCollection($manifest->getTestManifests());
-
-                        $test = $tests[0];
-                        $test->setState(Test::STATE_COMPLETE);
-
-                        $testStore = $services[TestStore::class];
-                        if ($testStore instanceof TestStore) {
-                            $testStore->store($test);
-                        }
-                    }
-                },
+                'setup' => new InvokableCollection([
+                    JobSetupInvokableFactory::setup(
+                        (new JobSetup())
+                            ->withSources([
+                                'Test/test1.yml',
+                                'Test/test2.yml',
+                            ])
+                    ),
+                    TestSetupInvokableFactory::setupCollection([
+                        (new TestSetup())
+                            ->withSource('/app/source/Test/test1.yml')
+                            ->withState(Test::STATE_COMPLETE),
+                        (new TestSetup())
+                            ->withSource('/app/source/Test/test2.yml'),
+                    ])
+                ]),
                 'expectedState' => new JobState(JobState::STATE_EXECUTION_RUNNING),
             ],
             'execution-complete' => [
-                'setup' => function (array $services) {
-                    $jobStore = $services[JobStore::class];
-                    if ($jobStore instanceof JobStore) {
-                        $job = $jobStore->create('', '');
-                        $job->setSources([
-                            'Test/test1.yml',
-                        ]);
-                        $jobStore->store($job);
-                    }
-
-                    $testManifests = [
-                        new TestManifest(
-                            new Configuration('chrome', 'http://example.com'),
-                            '/app/source/Test/test1.yml',
-                            '/app/tests/GeneratedTest1.php',
-                            1
-                        ),
-                    ];
-
-                    $manifest = (new MockSuiteManifest())
-                        ->withGetTestManifestsCall($testManifests)
-                        ->getMock();
-
-                    $testFactory = $services[TestFactory::class];
-                    if ($testFactory instanceof TestFactory) {
-                        $tests = $testFactory->createFromManifestCollection($manifest->getTestManifests());
-
-                        $test = $tests[0];
-                        $test->setState(Test::STATE_COMPLETE);
-
-                        $testStore = $services[TestStore::class];
-                        if ($testStore instanceof TestStore) {
-                            $testStore->store($test);
-                        }
-                    }
-                },
+                'setup' => new InvokableCollection([
+                    JobSetupInvokableFactory::setup(
+                        (new JobSetup())
+                            ->withSources([
+                                'Test/test1.yml',
+                            ])
+                    ),
+                    TestSetupInvokableFactory::setupCollection([
+                        (new TestSetup())
+                            ->withSource('/app/source/Test/test1.yml')
+                            ->withState(Test::STATE_COMPLETE),
+                    ])
+                ]),
                 'expectedState' => new JobState(JobState::STATE_EXECUTION_COMPLETE),
             ],
             'execution-cancelled: test.state == failed > 0' => [
-                'setup' => function (array $services) {
-                    $jobStore = $services[JobStore::class];
-                    if ($jobStore instanceof JobStore) {
-                        $job = $jobStore->create('', '');
-                        $job->setSources([
-                            'Test/test1.yml',
-                        ]);
-                        $jobStore->store($job);
-                    }
-
-                    $testManifests = [
-                        new TestManifest(
-                            new Configuration('chrome', 'http://example.com'),
-                            '/app/source/Test/test1.yml',
-                            '/app/tests/GeneratedTest1.php',
-                            1
-                        ),
-                    ];
-
-                    $manifest = (new MockSuiteManifest())
-                        ->withGetTestManifestsCall($testManifests)
-                        ->getMock();
-
-                    $testFactory = $services[TestFactory::class];
-                    if ($testFactory instanceof TestFactory) {
-                        $tests = $testFactory->createFromManifestCollection($manifest->getTestManifests());
-
-                        $test = $tests[0];
-                        $test->setState(Test::STATE_FAILED);
-
-                        $testStore = $services[TestStore::class];
-                        if ($testStore instanceof TestStore) {
-                            $testStore->store($test);
-                        }
-                    }
-                },
+                'setup' => new InvokableCollection([
+                    JobSetupInvokableFactory::setup(
+                        (new JobSetup())
+                            ->withSources([
+                                'Test/test1.yml',
+                            ])
+                    ),
+                    TestSetupInvokableFactory::setupCollection([
+                        (new TestSetup())
+                            ->withSource('/app/source/Test/test1.yml')
+                            ->withState(Test::STATE_FAILED),
+                    ])
+                ]),
                 'expectedState' => new JobState(JobState::STATE_EXECUTION_CANCELLED),
             ],
             'execution-cancelled: test.state == cancelled > 0' => [
-                'setup' => function (array $services) {
-                    $jobStore = $services[JobStore::class];
-                    if ($jobStore instanceof JobStore) {
-                        $job = $jobStore->create('', '');
-                        $job->setSources([
-                            'Test/test1.yml',
-                        ]);
-                        $jobStore->store($job);
-                    }
-
-                    $testManifests = [
-                        new TestManifest(
-                            new Configuration('chrome', 'http://example.com'),
-                            '/app/source/Test/test1.yml',
-                            '/app/tests/GeneratedTest1.php',
-                            1
-                        ),
-                    ];
-
-                    $manifest = (new MockSuiteManifest())
-                        ->withGetTestManifestsCall($testManifests)
-                        ->getMock();
-
-                    $testFactory = $services[TestFactory::class];
-                    if ($testFactory instanceof TestFactory) {
-                        $tests = $testFactory->createFromManifestCollection($manifest->getTestManifests());
-
-                        $test = $tests[0];
-                        $test->setState(Test::STATE_CANCELLED);
-
-                        $testStore = $services[TestStore::class];
-                        if ($testStore instanceof TestStore) {
-                            $testStore->store($test);
-                        }
-                    }
-                },
+                'setup' => new InvokableCollection([
+                    JobSetupInvokableFactory::setup(
+                        (new JobSetup())
+                            ->withSources([
+                                'Test/test1.yml',
+                            ])
+                    ),
+                    TestSetupInvokableFactory::setupCollection([
+                        (new TestSetup())
+                            ->withSource('/app/source/Test/test1.yml')
+                            ->withState(Test::STATE_CANCELLED),
+                    ])
+                ]),
                 'expectedState' => new JobState(JobState::STATE_EXECUTION_CANCELLED),
             ],
         ];
