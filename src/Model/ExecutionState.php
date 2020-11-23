faf38a0f9be6ce4b51526c6641555aa18f01bcc0
Jon Cram <webignition@gmail.com>
2020-11-23 17:20:59 +0000

    Create ExecutionState model

---
+++ src/Model/ExecutionState.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model;
+
+class ExecutionState
+{
+    public const STATE_AWAITING = 'awaiting';
+    public const STATE_RUNNING = 'running';
+    public const STATE_FAILED = 'failed';
+    public const STATE_COMPLETE = 'complete';
+    public const STATE_CANCELLED = 'cancelled';
+    public const STATE_UNKNOWN = 'unknown';
+
+    /**
+     * @var ExecutionState::STATE_*
+     */
+    private string $state;
+
+    /**
+     * @param ExecutionState::STATE_* $state
+     */
+    public function __construct(string $state)
+    {
+        $this->state = $state;
+    }
+
+    public function __toString(): string
+    {
+        return $this->state;
+    }
+}
