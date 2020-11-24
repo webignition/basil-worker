d40bf445cd797b1e1525c648592f229f6df30e26
Jon Cram <webignition@gmail.com>
2020-11-24 11:37:32 +0000

    Remove JobState::isRunning() (#567)

--- src/Model/JobState.php
+++ src/Model/JobState.php
@@ -16,11 +16,6 @@ class JobState
     public const STATE_EXECUTION_CANCELLED = 'execution-cancelled';
     public const STATE_UNKNOWN = 'unknown';
 
-    private const RUNNING_STATES = [
-        self::STATE_COMPILATION_RUNNING,
-        self::STATE_EXECUTION_RUNNING,
-    ];
-
     /**
      * @var JobState::STATE_*
      */
@@ -32,11 +27,6 @@ class JobState
     public function __construct(string $state)
     {
         $this->state = $state;
-    }
-
-    public function isRunning(): bool
-    {
-        return in_array($this->state, self::RUNNING_STATES);
     }
 
     public function __toString(): string
