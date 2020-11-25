6580a1c636b13e957c39e366909ee92f7032ab52
Jon Cram <webignition@gmail.com>
2020-11-25 17:55:46 +0000

    Dispatch first TimeoutCheck message

--- src/MessageHandler/TimeoutCheckHandler.php
+++ src/MessageHandler/TimeoutCheckHandler.php
@@ -6,32 +6,25 @@ namespace App\MessageHandler;
 
 use App\Event\JobTimeoutEvent;
 use App\Message\TimeoutCheck;
+use App\MessageDispatcher\TimeoutCheckMessageDispatcher;
 use App\Services\JobStore;
 use Psr\EventDispatcher\EventDispatcherInterface;
-use Symfony\Component\Messenger\Envelope;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
-use Symfony\Component\Messenger\MessageBusInterface;
-use Symfony\Component\Messenger\Stamp\DelayStamp;
 
 class TimeoutCheckHandler implements MessageHandlerInterface
 {
-    private const MILLISECONDS_PER_SECOND = 1000;
-
     private JobStore $jobStore;
-    private MessageBusInterface $messageBus;
+    private TimeoutCheckMessageDispatcher $timeoutCheckMessageDispatcher;
     private EventDispatcherInterface $eventDispatcher;
-    private int $recheckPeriodInSeconds;
 
     public function __construct(
         JobStore $jobStore,
-        MessageBusInterface $messageBus,
-        EventDispatcherInterface $eventDispatcher,
-        int $recheckPeriodInSeconds
+        TimeoutCheckMessageDispatcher $timeoutCheckMessageDispatcher,
+        EventDispatcherInterface $eventDispatcher
     ) {
         $this->jobStore = $jobStore;
-        $this->messageBus = $messageBus;
+        $this->timeoutCheckMessageDispatcher = $timeoutCheckMessageDispatcher;
         $this->eventDispatcher = $eventDispatcher;
-        $this->recheckPeriodInSeconds = $recheckPeriodInSeconds;
     }
 
     public function __invoke(TimeoutCheck $timeoutCheck): void
@@ -44,11 +37,7 @@ class TimeoutCheckHandler implements MessageHandlerInterface
         if ($job->hasReachedMaximumDuration()) {
             $this->eventDispatcher->dispatch(new JobTimeoutEvent($job->getMaximumDuration()));
         } else {
-            $message = new TimeoutCheck();
-            $envelope = new Envelope($message, [
-                new DelayStamp($this->recheckPeriodInSeconds * self::MILLISECONDS_PER_SECOND)
-            ]);
-            $this->messageBus->dispatch($envelope);
+            $this->timeoutCheckMessageDispatcher->dispatch();
         }
     }
 }
