2a5e5e134e07f4ecf77967c3cfed1c3441c6e0e9
Jon Cram <webignition@gmail.com>
2020-12-04 10:49:14 +0000

    Create Source entity (#662)

---
+++ src/Repository/SourceRepository.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Source;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method Source|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Source|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Source[]    findAll()
+ * @method Source[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ *
+ * @extends \Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository<Source>
+ */
+class SourceRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Source::class);
+    }
+}
