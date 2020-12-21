ff83dd5d8e19cb3d80b37ebb813e4f66342a1806
Jon Cram <webignition@gmail.com>
2020-12-21 16:01:47 +0000

    Create VM (#737)

---
+++ Vagrantfile
@@ -0,0 +1,20 @@
+host_port = ENV["HOST_PORT"] || 8080
+
+Vagrant.configure("2") do |config|
+  config.vm.define "basil_worker" do |basil_worker|
+    basil_worker.vm.box = "focal-server-cloudimg-amd64-vagrant"
+    basil_worker.vm.box_url = "https://cloud-images.ubuntu.com/focal/current/focal-server-cloudimg-amd64-vagrant.box"
+
+    basil_worker.vm.provider "virtualbox" do |v|
+      v.name = "basil_worker"
+    end
+
+    basil_worker.vm.network "forwarded_port", guest: 80, host: host_port
+
+    basil_worker.vm.provision "file", source: "./.docker-compose.env", destination: "~/.docker-compose.env"
+    basil_worker.vm.provision "file", source: "./docker-compose.yml", destination: "~/docker-compose.yml"
+    basil_worker.vm.provision "file", source: "./nginx/Dockerfile", destination: "~/nginx/Dockerfile"
+    basil_worker.vm.provision "file", source: "./nginx/site.conf", destination: "~/nginx/site.conf"
+    basil_worker.vm.provision "shell", path: "provision.sh"
+  end
+end
---
+++ provision.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+sudo apt-get update && apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update && apt-get install -y \
+    docker-ce \
+    docker-ce-cli \
+    containerd.io
+
+apt-get autoremove -y \
+  && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+
+DOCKER_COMPOSE_BIN=/usr/local/bin/docker-compose
+if [ ! -f "$DOCKER_COMPOSE_BIN" ]; then
+  sudo curl \
+    -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" \
+    -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+fi
+
+mkdir -p /var/basil/source
+mkdir -p /var/basil/tests
+
+sudo docker-compose --env-file .docker-compose.env up -d
+sudo docker-compose --env-file .docker-compose.env exec -T app-handler php bin/console doctrine:database:create --if-not-exists
+sudo docker-compose --env-file .docker-compose.env exec -T app-handler php bin/console doctrine:migrations:migrate --no-interaction
