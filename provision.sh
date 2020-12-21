e0f7602f989680f1a87792796fa49c64394c11f0
Jon Cram <webignition@gmail.com>
2020-12-21 11:00:26 +0000

    Run DB migrations

--- provision.sh
+++ provision.sh
@@ -34,3 +34,4 @@ mkdir -p /var/basil/source
 mkdir -p /var/basil/tests
 
 sudo docker-compose --env-file .docker-compose.env up -d
+sudo docker-compose --env-file .docker-compose.env exec -T app-handler php bin/console doctrine:database:create --if-not-exists
