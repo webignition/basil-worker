c345eefd14e54889b2976eb77f6bcc54c4f73097
Jon Cram <webignition@gmail.com>
2020-12-22 15:55:31 +0000

    Remove created snapshot

--- delete-snapshot.sh
+++ delete-snapshot.sh
@@ -9,7 +9,7 @@ EXIT_CODE_SNAPSHOT_ID_INCORRECT=3
 AUTH_HEADER="Authorization: Bearer ${AUTH}"
 URL="https://api.digitalocean.com/v2/snapshots/${ID}"
 
-RESPONSE_JSON=$(curl -s -X GET -H 'Content-Type: application/json' -H "${AUTH_HEADER}" "${URL}")
+RESPONSE_JSON=$(curl -s -X DELETE -H 'Content-Type: application/json' -H "${AUTH_HEADER}" "${URL}")
 
 RESPONSE_JSON_HAS_SNAPSHOT=$(echo $RESPONSE_JSON | jq 'has("snapshot")')
 if [ $RESPONSE_JSON_HAS_SNAPSHOT != "true" ]; then
@@ -27,5 +27,3 @@ EXPECTED_RESPONSE_ID="\"${ID}\""
 if [ $RESPONSE_ID != $EXPECTED_RESPONSE_ID ]; then
   exit $EXIT_CODE_SNAPSHOT_ID_INCORRECT
 fi
-
-echo "Snapshot ${ID} exists ✓"
