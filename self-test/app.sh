a20a4ec6bb2d09a8cfb0c7510bc7db0cdf8b4a33
Jon Cram <webignition@gmail.com>
2021-05-18 16:49:08 +0100

    shellcheck new bash scripts

--- self-test/app.sh
+++ self-test/app.sh
@@ -1,41 +1,37 @@
 #!/usr/bin/env bash
 
-PACKAGES="php7.4-cli php7.4-curl php7.4-dom php7.4-mbstring zip"
-
-INITIAL_DIRECTORY=$(echo $PWD)
+INITIAL_DIRECTORY=$PWD
 
 # Setup
 sudo docker-compose --env-file .docker-compose.env -f docker-compose.yml -f self-test/services.yml build
 sudo docker-compose --env-file .docker-compose.env -f docker-compose.yml -f self-test/services.yml up -d
 
-cd ./self-test/app
+cd ./self-test/app || exit
 
 sudo apt-get -qq update > /dev/null
-sudo apt-get -qq install ${PACKAGES} > /dev/null
+sudo apt-get -qq install php7.4-cli php7.4-curl php7.4-dom php7.4-mbstring zip > /dev/null
 curl https://getcomposer.org/download/latest-stable/composer.phar --output composer.phar --silent
 
-php composer.phar update --quiet
-if [ 0 -ne $? ]; then
-    exit $?
+if ! php composer.phar update --quiet; then
+    exit 1
 fi
 
-php composer.phar check-platform-reqs --quiet
-if [ 0 -ne $? ]; then
-    exit $?
+if ! php composer.phar check-platform-reqs --quiet; then
+    exit 1
 fi
 
 # Run
 php ./vendor/bin/phpunit ./src/ApplicationTest.php
 LAST_EXIT_CODE=$?
 
-if [ ${LAST_EXIT_CODE} -eq 0 ]; then
+if [ "$LAST_EXIT_CODE" -eq 0 ]; then
     sleep 10
     sudo docker logs callback-receiver | php ./vendor/bin/phpunit ./src/CallbackReceiverLogTest.php
     LAST_EXIT_CODE=$?
 fi
 
 ## Teardown
-cd $INITIAL_DIRECTORY
+cd "$INITIAL_DIRECTORY" || exit
 sudo docker-compose --env-file .docker-compose.env -f docker-compose.yml -f self-test/services.yml stop http-fixtures
 sudo docker-compose --env-file .docker-compose.env -f docker-compose.yml -f self-test/services.yml stop callback-receiver
 sudo docker-compose --env-file .docker-compose.env up -d --remove-orphans
@@ -50,14 +46,14 @@ DB_TABLES=(
 
 for TABLE in ${DB_TABLES[*]}
   do
-    echo "Removing all from ${TABLE}"
+    echo "Removing all from $TABLE"
     sudo docker-compose --env-file .docker-compose.env exec -T -e PGPASSWORD=password! postgres psql -U postgres -d worker-db -c "DELETE FROM ${TABLE}"
   done
 
-sudo apt-get -qq -y remove ${PACKAGES} > /dev/null
+sudo apt-get -qq -y remove php7.4-cli php7.4-curl php7.4-dom php7.4-mbstring zip > /dev/null
 sudo apt-get -qq -y autoremove > /dev/null
 sudo rm -Rf ./self-test
 
-if [ 0 -ne ${LAST_EXIT_CODE} ]; then
-    exit ${LAST_EXIT_CODE}
+if [ 0 -ne "$LAST_EXIT_CODE" ]; then
+    exit 1
 fi
--- self-test/delegator.sh
+++ self-test/delegator.sh
@@ -2,11 +2,6 @@
 
 BROWSER=$1
 
-EXIT_CODE_COMPILER_FAILED=100
-EXIT_CODE_RUNNER_FAILED=101
-EXIT_CODE_LOCAL_SOURCE_PATH_NOT_EMPTY=102
-EXIT_CODE_LOCAL_COMPILED_TARGET=103
-
 ICON_FAILED="𐄂"
 ICON_PASSED="✓"
 
@@ -16,23 +11,24 @@ LOCAL_TARGET_PATH="/var/basil/tests"
 TEST_FILENAME="test-${BROWSER}.yml"
 
 sed "s/{{ BROWSER }}/${BROWSER}/g" ./self-test/fixtures/basil/test.yml | sudo tee "${LOCAL_SOURCE_PATH}/${TEST_FILENAME}"
-COMPILE_OUTPUT=$(sudo docker-compose --env-file .docker-compose.env exec -T compiler ./compiler --source=/app/source/${TEST_FILENAME} --target=/app/tests)
-if [ $? -ne 0 ]; then
+COMPILE_OUTPUT=$(sudo docker-compose --env-file .docker-compose.env exec -T compiler ./compiler --source=/app/source/"${TEST_FILENAME}" --target=/app/tests)
+COMPILE_EXIT_CODE=$?
+if [ "${COMPILE_EXIT_CODE}" -ne 0 ]; then
     echo "${ICON_FAILED} compiler test failed"
-    exit ${EXIT_CODE_COMPILER_FAILED}
+    exit 1
 fi
 echo "${ICON_PASSED} compiler test passed"
 
 COMPILED_TARGET_LINE=$(echo -e "${COMPILE_OUTPUT}" | grep "/app/tests/Generated")
 COMPILED_TARGET=$(echo "${COMPILED_TARGET_LINE/target: /}" | xargs)
 
-NGINX_FIXTURE_PATH=$(echo "${PWD}/self-test/fixtures/http")
-docker run --name http-fixtures --network worker-network -p 8080:80 -v ${NGINX_FIXTURE_PATH}:/usr/share/nginx/html:ro -d nginx:1.19
+docker run --name http-fixtures --network worker-network -p 8080:80 -v "${PWD}/self-test/fixtures/http":/usr/share/nginx/html:ro -d nginx:1.19
 
-sudo docker-compose --env-file .docker-compose.env exec -T delegator ./bin/delegator  --browser ${BROWSER} ${COMPILED_TARGET}
-if [ $? -ne 0 ]; then
+sudo docker-compose --env-file .docker-compose.env exec -T delegator ./bin/delegator  --browser "${BROWSER}" "${COMPILED_TARGET}"
+DELEGATOR_EXIT_CODE=$?
+if [ "$DELEGATOR_EXIT_CODE" -ne 0 ]; then
     echo "${ICON_FAILED} ${BROWSER} delegator test failed"
-    exit ${EXIT_CODE_RUNNER_FAILED}
+    exit 1
 fi
 echo "${ICON_PASSED} ${BROWSER} delegator test passed"
 
@@ -41,19 +37,19 @@ sudo rm ${LOCAL_SOURCE_PATH}/*.yml
 sudo rm ${LOCAL_TARGET_PATH}/*.php
 
 # Verify local source path is empty
-LOCAL_SOURCE_PATH_FILE_COUNT=$(ls -A /var/basil/source | wc -l)
-if [ $LOCAL_SOURCE_PATH_FILE_COUNT -eq 0 ]; then
+LOCAL_SOURCE_PATH_FILE_COUNT=$(find "$LOCAL_SOURCE_PATH" | grep '\.yml' -c)
+if [ "$LOCAL_SOURCE_PATH_FILE_COUNT" -eq 0 ]; then
     echo "${ICON_PASSED} ${LOCAL_SOURCE_PATH} is empty";
 else
     echo "${ICON_FAILED} ${LOCAL_SOURCE_PATH} is not empty";
-    exit ${EXIT_CODE_LOCAL_SOURCE_PATH_NOT_EMPTY}
+    exit 1
 fi
 
 # Verify local target path is empty
-TESTS_PATH_FILE_COUNT=$(ls -A /var/basil/tests | wc -l)
-if [ $TESTS_PATH_FILE_COUNT -eq 0 ]; then
+TESTS_PATH_FILE_COUNT=$(find "$LOCAL_TARGET_PATH" | grep '\.php' -c)
+if [ "$TESTS_PATH_FILE_COUNT" -eq 0 ]; then
     echo "${ICON_PASSED} ${LOCAL_TARGET_PATH} is empty";
 else
     echo "${ICON_FAILED} ${LOCAL_TARGET_PATH} is not empty";
-    exit ${EXIT_CODE_LOCAL_TARGET_PATH_NOT_EMPTY}
+    exit 1
 fi
