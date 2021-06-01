72accecb1f4acfea88f1bcb4afe957c5c9189977
Jon Cram <webignition@gmail.com>
2021-06-01 10:44:36 +0100

    Fix CS issues following master rebase (#909)

--- src/Model/BackoffStrategy/BackoffStrategyInterface.php
+++ src/Model/BackoffStrategy/BackoffStrategyInterface.php
@@ -7,8 +7,6 @@ namespace App\Model\BackoffStrategy;
 interface BackoffStrategyInterface
 {
     /**
-     * @param int $retryCount
-     *
      * @return int Delay in milliseconds
      */
     public function getDelay(int $retryCount): int;
