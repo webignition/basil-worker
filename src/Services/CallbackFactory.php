2c8853e8e97054b63969e930922006cad61c37b3
Jon Cram <webignition@gmail.com>
2021-06-02 10:54:52 +0100

    Import CallbackFactory service

---
+++ src/Services/CallbackFactory.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services;
+
+use App\Services\EventCallbackFactory\EventCallbackFactoryInterface;
+use Symfony\Contracts\EventDispatcher\Event;
+use webignition\BasilWorker\PersistenceBundle\Entity\Callback\CallbackInterface;
+
+class CallbackFactory
+{
+    /**
+     * @var EventCallbackFactoryInterface[]
+     */
+    private array $eventCallbackFactories;
+
+    /**
+     * @param array<mixed> $eventCallbackFactories
+     */
+    public function __construct(
+        array $eventCallbackFactories
+    ) {
+        $this->eventCallbackFactories = array_filter($eventCallbackFactories, function ($item) {
+            return $item instanceof EventCallbackFactoryInterface;
+        });
+    }
+
+    public function createForEvent(Event $event): ?CallbackInterface
+    {
+        foreach ($this->eventCallbackFactories as $eventCallbackFactory) {
+            if ($eventCallbackFactory->handles($event)) {
+                return $eventCallbackFactory->createForEvent($event);
+            }
+        }
+
+        return null;
+    }
+}
