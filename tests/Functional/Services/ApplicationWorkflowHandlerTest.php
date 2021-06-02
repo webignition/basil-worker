71b4f7acc86b09dfc46c9e35b35ca4f7c2897f4e
Jon Cram <webignition@gmail.com>
2021-06-02 15:06:16 +0100

    Import ApplicationWorkflowHandler service (#923)

---
+++ tests/Functional/Services/ApplicationWorkflowHandlerTest.php
@@ -0,0 +1,165 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Functional\Services;
+
+use App\Event\JobCompletedEvent;
+use App\Event\JobFailedEvent;
+// todo: uncomment in #888
+// use App\Event\JobReadyEvent;
+use App\Event\TestFailedEvent;
+use App\Event\TestPassedEvent;
+use App\Message\JobCompletedCheckMessage;
+use App\MessageDispatcher\SendCallbackMessageDispatcher;
+use App\Services\ApplicationWorkflowHandler;
+// todo: uncomment in #888
+//use App\Services\ExecutionWorkflowHandler;
+use App\Tests\AbstractBaseFunctionalTest;
+use App\Tests\Mock\Entity\MockTest;
+use App\Tests\Mock\MockEventDispatcher;
+use App\Tests\Mock\Services\MockApplicationState;
+use App\Tests\Model\ExpectedDispatchedEvent;
+use App\Tests\Model\ExpectedDispatchedEventCollection;
+use App\Tests\Services\Asserter\MessengerAsserter;
+use App\Tests\Services\EventListenerRemover;
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Contracts\EventDispatcher\Event;
+use webignition\BasilWorker\StateBundle\Services\ApplicationState;
+use webignition\ObjectReflector\ObjectReflector;
+use webignition\YamlDocument\Document;
+
+class ApplicationWorkflowHandlerTest extends AbstractBaseFunctionalTest
+{
+    use MockeryPHPUnitIntegration;
+
+    private ApplicationWorkflowHandler $handler;
+    private EventDispatcherInterface $eventDispatcher;
+    private MessengerAsserter $messengerAsserter;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $applicationWorkflowHandler = self::$container->get(ApplicationWorkflowHandler::class);
+        \assert($applicationWorkflowHandler instanceof ApplicationWorkflowHandler);
+        $this->handler = $applicationWorkflowHandler;
+
+        $eventDispatcher = self::$container->get(EventDispatcherInterface::class);
+        \assert($eventDispatcher instanceof EventDispatcherInterface);
+        $this->eventDispatcher = $eventDispatcher;
+
+        $messengerAsserter = self::$container->get(MessengerAsserter::class);
+        \assert($messengerAsserter instanceof MessengerAsserter);
+        $this->messengerAsserter = $messengerAsserter;
+
+        $eventListenerRemover = self::$container->get(EventListenerRemover::class);
+        \assert($eventListenerRemover instanceof EventListenerRemover);
+        $eventListenerRemover->remove([
+            SendCallbackMessageDispatcher::class => [
+                TestPassedEvent::class => ['dispatchForEvent'],
+                TestFailedEvent::class => ['dispatchForEvent'],
+            ],
+            // todo: uncomment in #888
+            //            ExecutionWorkflowHandler::class => [
+            //                TestPassedEvent::class => [
+            //                    'dispatchExecutionCompletedEvent',
+            //                    'dispatchNextExecuteTestMessageFromTestPassedEvent',
+            //                ],
+            //            ],
+        ]);
+    }
+
+    public function testSubscribesToTestPassedEventApplicationNotComplete(): void
+    {
+        $eventDispatcher = (new MockEventDispatcher())
+            ->withoutDispatchCall()
+            ->getMock()
+        ;
+
+        ObjectReflector::setProperty(
+            $this->handler,
+            ApplicationWorkflowHandler::class,
+            'eventDispatcher',
+            $eventDispatcher
+        );
+
+        $this->messengerAsserter->assertQueueIsEmpty();
+
+        $this->eventDispatcher->dispatch(new TestPassedEvent(
+            (new MockTest())->getMock(),
+            new Document(''),
+        ));
+
+        $this->messengerAsserter->assertQueueCount(1);
+        $this->messengerAsserter->assertMessageAtPositionEquals(
+            0,
+            new JobCompletedCheckMessage()
+        );
+    }
+
+    public function testSubscribesToTestPassedEventApplicationComplete(): void
+    {
+        $eventDispatcher = (new MockEventDispatcher())
+            ->withDispatchCalls(new ExpectedDispatchedEventCollection([
+                new ExpectedDispatchedEvent(function (Event $event) {
+                    self::assertInstanceOf(JobCompletedEvent::class, $event);
+
+                    return true;
+                }),
+            ]))
+            ->getMock()
+        ;
+
+        ObjectReflector::setProperty(
+            $this->handler,
+            ApplicationWorkflowHandler::class,
+            'eventDispatcher',
+            $eventDispatcher
+        );
+
+        $applicationState = (new MockApplicationState())
+            ->withIsCall(true, ApplicationState::STATE_COMPLETE)
+            ->getMock()
+        ;
+
+        ObjectReflector::setProperty(
+            $this->handler,
+            ApplicationWorkflowHandler::class,
+            'applicationState',
+            $applicationState
+        );
+
+        $this->eventDispatcher->dispatch(new TestPassedEvent(
+            (new MockTest())->getMock(),
+            new Document(''),
+        ));
+    }
+
+    public function testSubscribesToTestFailedEvent(): void
+    {
+        $eventDispatcher = (new MockEventDispatcher())
+            ->withDispatchCalls(new ExpectedDispatchedEventCollection([
+                new ExpectedDispatchedEvent(function (Event $event) {
+                    self::assertInstanceOf(JobFailedEvent::class, $event);
+
+                    return true;
+                }),
+            ]))
+            ->getMock()
+        ;
+
+        ObjectReflector::setProperty(
+            $this->handler,
+            ApplicationWorkflowHandler::class,
+            'eventDispatcher',
+            $eventDispatcher
+        );
+
+        $this->eventDispatcher->dispatch(new TestFailedEvent(
+            (new MockTest())->getMock(),
+            new Document(''),
+        ));
+    }
+}
