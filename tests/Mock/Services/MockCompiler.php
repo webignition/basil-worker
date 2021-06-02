1a28ed9808f9e762195a274f30aeaa508f932d7a
Jon Cram <webignition@gmail.com>
2021-06-02 18:05:19 +0100

    Import CompileSourceHandler (#936)

---
+++ tests/Mock/Services/MockCompiler.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Mock\Services;
+
+use App\Services\Compiler;
+use Mockery\MockInterface;
+use webignition\BasilCompilerModels\OutputInterface;
+
+class MockCompiler
+{
+    private Compiler $mock;
+
+    public function __construct()
+    {
+        $this->mock = \Mockery::mock(Compiler::class);
+    }
+
+    public function getMock(): Compiler
+    {
+        return $this->mock;
+    }
+
+    public function withCompileCall(string $source, OutputInterface $output): self
+    {
+        if (false === $this->mock instanceof MockInterface) {
+            return $this;
+        }
+
+        $this->mock
+            ->shouldReceive('compile')
+            ->with($source)
+            ->andReturn($output)
+        ;
+
+        return $this;
+    }
+}
