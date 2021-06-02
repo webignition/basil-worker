11312258c4817e4ca8cd1edcd2786b581488f0b0
Jon Cram <webignition@gmail.com>
2021-06-02 11:04:01 +0100

    Import CallbackFactory service (#919)

---
+++ tests/Functional/Services/CallbackFactoryTest.php
@@ -0,0 +1,80 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Functional\Services;
+
+use App\Services\CallbackFactory;
+use App\Tests\AbstractBaseFunctionalTest;
+use App\Tests\DataProvider\CallbackFactory\CreateFromCompilationCompletedEventDataProviderTrait;
+use App\Tests\DataProvider\CallbackFactory\CreateFromCompilationFailedEventDataProviderTrait;
+use App\Tests\DataProvider\CallbackFactory\CreateFromCompilationPassedEventDataProviderTrait;
+use App\Tests\DataProvider\CallbackFactory\CreateFromCompilationStartedEventDataProviderTrait;
+use App\Tests\DataProvider\CallbackFactory\CreateFromExecutionCompletedEventDataProviderTrait;
+use App\Tests\DataProvider\CallbackFactory\CreateFromExecutionStartedEventDataProviderTrait;
+use App\Tests\DataProvider\CallbackFactory\CreateFromJobCompletedEventDataProviderTrait;
+use App\Tests\DataProvider\CallbackFactory\CreateFromJobFailedEventDataProviderTrait;
+use App\Tests\DataProvider\CallbackFactory\CreateFromJobReadyEventDataProviderTrait;
+use App\Tests\DataProvider\CallbackFactory\CreateFromJobTimeoutEventDataProviderTrait;
+use App\Tests\DataProvider\CallbackFactory\CreateFromTestEventDataProviderTrait;
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use Symfony\Contracts\EventDispatcher\Event;
+use webignition\BasilWorker\PersistenceBundle\Entity\Callback\CallbackInterface;
+
+class CallbackFactoryTest extends AbstractBaseFunctionalTest
+{
+    use MockeryPHPUnitIntegration;
+    use CreateFromCompilationStartedEventDataProviderTrait;
+    use CreateFromCompilationPassedEventDataProviderTrait;
+    use CreateFromCompilationFailedEventDataProviderTrait;
+    use CreateFromTestEventDataProviderTrait;
+    use CreateFromJobTimeoutEventDataProviderTrait;
+    use CreateFromJobCompletedEventDataProviderTrait;
+    use CreateFromJobReadyEventDataProviderTrait;
+    use CreateFromCompilationCompletedEventDataProviderTrait;
+    use CreateFromExecutionStartedEventDataProviderTrait;
+    use CreateFromExecutionCompletedEventDataProviderTrait;
+    use CreateFromJobFailedEventDataProviderTrait;
+
+    private CallbackFactory $callbackFactory;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $callbackFactory = self::$container->get(CallbackFactory::class);
+        \assert($callbackFactory instanceof CallbackFactory);
+        $this->callbackFactory = $callbackFactory;
+    }
+
+    public function testCreateForEventUnsupportedEvent(): void
+    {
+        self::assertNull($this->callbackFactory->createForEvent(new Event()));
+    }
+
+    /**
+     * @dataProvider createFromCompilationStartedEventDataProvider
+     * @dataProvider createFromCompilationPassedEventDataProvider
+     * @dataProvider createFromCompilationFailedEventDataProvider
+     * @dataProvider createFromCompilationCompletedEventDataProvider
+     * @dataProvider createFromExecutionStartedEventDataProvider
+     * @dataProvider createFromTestEventEventDataProvider
+     * @dataProvider createFromJobTimeoutEventDataProvider
+     * @dataProvider createFromJobCompletedEventDataProvider
+     * @dataProvider createFromJobReadyEventDataProvider
+     * @dataProvider createFromExecutionCompletedEventDataProvider
+     * @dataProvider createFromJobFailedEventDataProvider
+     */
+    public function testCreateForEvent(Event $event, CallbackInterface $expectedCallback): void
+    {
+        $callback = $this->callbackFactory->createForEvent($event);
+
+        self::assertInstanceOf(CallbackInterface::class, $callback);
+
+        if ($callback instanceof CallbackInterface) {
+            self::assertNotNull($callback->getId());
+            self::assertSame($expectedCallback->getType(), $callback->getType());
+            self::assertSame($expectedCallback->getPayload(), $callback->getPayload());
+        }
+    }
+}
