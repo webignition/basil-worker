46bf1b39b37c7509d69c188dcbeab074a095062c
Jon Cram <webignition@gmail.com>
2021-06-02 15:43:59 +0100

    Import CompilationWorkflowHandler service

---
+++ src/Message/CompileSourceMessage.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Message;
+
+use webignition\JsonMessageSerializerBundle\Message\AbstractSerializableMessage;
+
+class CompileSourceMessage extends AbstractSerializableMessage
+{
+    public const TYPE = 'compile-source';
+    public const PAYLOAD_KEY_PATH = 'path';
+
+    public function __construct(private string $path)
+    {
+    }
+
+    /**
+     * @param array<mixed> $data
+     */
+    public static function createFromArray(array $data): self
+    {
+        return new CompileSourceMessage((string) ($data[self::PAYLOAD_KEY_PATH] ?? ''));
+    }
+
+    public function getPath(): string
+    {
+        return $this->path;
+    }
+
+    public function getType(): string
+    {
+        return self::TYPE;
+    }
+
+    public function getPayload(): array
+    {
+        return [
+            self::PAYLOAD_KEY_PATH => $this->path,
+        ];
+    }
+}
