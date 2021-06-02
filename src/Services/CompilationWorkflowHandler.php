749b05287f1e45adbaf463b640e88e9546f642ff
Jon Cram <webignition@gmail.com>
2021-06-02 15:57:01 +0100

    Import CompilationWorkflowHandler service (#925)

---
+++ src/Services/CompilationWorkflowHandler.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services;
+
+use App\Event\CompilationCompletedEvent;
+use App\Event\JobReadyEvent;
+use App\Event\SourceCompilation\PassedEvent as SourceCompilationPassedEvent;
+use App\Message\CompileSourceMessage;
+use Psr\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Messenger\MessageBusInterface;
+use webignition\BasilWorker\StateBundle\Services\CompilationState;
+use webignition\BasilWorker\StateBundle\Services\SourcePathFinder;
+
+class CompilationWorkflowHandler implements EventSubscriberInterface
+{
+    public function __construct(
+        private CompilationState $compilationState,
+        private EventDispatcherInterface $eventDispatcher,
+        private MessageBusInterface $messageBus,
+        private SourcePathFinder $sourcePathFinder
+    ) {
+    }
+
+    /**
+     * @return array[][]
+     */
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            SourceCompilationPassedEvent::class => [
+                ['dispatchNextCompileSourceMessage', 50],
+                ['dispatchCompilationCompletedEvent', 60],
+            ],
+            JobReadyEvent::class => [
+                ['dispatchNextCompileSourceMessage', 50],
+            ],
+        ];
+    }
+
+    public function dispatchNextCompileSourceMessage(): void
+    {
+        if (false === $this->compilationState->is(...CompilationState::FINISHED_STATES)) {
+            $sourcePath = $this->sourcePathFinder->findNextNonCompiledPath();
+
+            if (is_string($sourcePath)) {
+                $this->messageBus->dispatch(new CompileSourceMessage($sourcePath));
+            }
+        }
+    }
+
+    public function dispatchCompilationCompletedEvent(): void
+    {
+        if (CompilationState::STATE_COMPLETE === (string) $this->compilationState) {
+            $this->eventDispatcher->dispatch(new CompilationCompletedEvent());
+        }
+    }
+}
---
+++ tests/Functional/Services/CompilationWorkflowHandlerTest.php
@@ -0,0 +1,192 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Functional\Services;
+
+use App\Event\JobReadyEvent;
+use App\Event\SourceCompilation\PassedEvent;
+use App\Message\CompileSourceMessage;
+use App\Message\TimeoutCheckMessage;
+use App\MessageDispatcher\SendCallbackMessageDispatcher;
+use App\Services\CompilationWorkflowHandler;
+// todo: uncomment in #888
+//use App\Services\ExecutionWorkflowHandler;
+use App\Tests\AbstractBaseFunctionalTest;
+use App\Tests\Mock\MockSuiteManifest;
+use App\Tests\Model\EnvironmentSetup;
+use App\Tests\Model\SourceSetup;
+use App\Tests\Model\TestSetup;
+use App\Tests\Services\Asserter\MessengerAsserter;
+use App\Tests\Services\EnvironmentFactory;
+use App\Tests\Services\EventListenerRemover;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Contracts\EventDispatcher\Event;
+
+class CompilationWorkflowHandlerTest extends AbstractBaseFunctionalTest
+{
+    private CompilationWorkflowHandler $handler;
+    private EventDispatcherInterface $eventDispatcher;
+    private MessengerAsserter $messengerAsserter;
+    private EnvironmentFactory $environmentFactory;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $compilationWorkflowHandler = self::$container->get(CompilationWorkflowHandler::class);
+        \assert($compilationWorkflowHandler instanceof CompilationWorkflowHandler);
+        $this->handler = $compilationWorkflowHandler;
+
+        $eventDispatcher = self::$container->get(EventDispatcherInterface::class);
+        \assert($eventDispatcher instanceof EventDispatcherInterface);
+        $this->eventDispatcher = $eventDispatcher;
+
+        $messengerAsserter = self::$container->get(MessengerAsserter::class);
+        \assert($messengerAsserter instanceof MessengerAsserter);
+        $this->messengerAsserter = $messengerAsserter;
+
+        $environmentFactory = self::$container->get(EnvironmentFactory::class);
+        \assert($environmentFactory instanceof EnvironmentFactory);
+        $this->environmentFactory = $environmentFactory;
+
+        $eventListenerRemover = self::$container->get(EventListenerRemover::class);
+        \assert($eventListenerRemover instanceof EventListenerRemover);
+        $eventListenerRemover->remove([
+            SendCallbackMessageDispatcher::class => [
+                JobReadyEvent::class => ['dispatchForEvent'],
+                PassedEvent::class => ['dispatchForEvent'],
+            ],
+            // todo: uncomment in #888
+            //            ExecutionWorkflowHandler::class => [
+            //                PassedEvent::class => ['dispatchExecutionStartedEvent'],
+            //            ],
+        ]);
+    }
+
+    /**
+     * @dataProvider dispatchNextCompileSourceMessageNoMessageDispatchedDataProvider
+     */
+    public function testDispatchNextCompileSourceMessageNoMessageDispatched(EnvironmentSetup $setup): void
+    {
+        $this->environmentFactory->create($setup);
+
+        $this->handler->dispatchNextCompileSourceMessage();
+
+        $this->messengerAsserter->assertQueueIsEmpty();
+    }
+
+    /**
+     * @return array[]
+     */
+    public function dispatchNextCompileSourceMessageNoMessageDispatchedDataProvider(): array
+    {
+        return [
+            'no sources' => [
+                'setup' => new EnvironmentSetup(),
+            ],
+            'no non-compiled sources' => [
+                'setup' => (new EnvironmentSetup())
+                    ->withTestSetups([
+                        (new TestSetup())->withSource('/app/source/Test/test1.yml')
+                    ]),
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider dispatchNextCompileSourceMessageMessageDispatchedDataProvider
+     */
+    public function testDispatchNextCompileSourceMessageMessageDispatched(
+        EnvironmentSetup $setup,
+        CompileSourceMessage $expectedQueuedMessage
+    ): void {
+        $this->environmentFactory->create($setup);
+
+        $this->handler->dispatchNextCompileSourceMessage();
+
+        $this->messengerAsserter->assertQueueCount(1);
+        $this->messengerAsserter->assertMessageAtPositionEquals(0, $expectedQueuedMessage);
+    }
+
+    /**
+     * @return array[]
+     */
+    public function dispatchNextCompileSourceMessageMessageDispatchedDataProvider(): array
+    {
+        return [
+            'no sources compiled' => [
+                'setup' => (new EnvironmentSetup())
+                    ->withSourceSetups([
+                        (new SourceSetup())->withPath('Test/test1.yml'),
+                        (new SourceSetup())->withPath('Test/test2.yml'),
+                    ]),
+                'expectedQueuedMessage' => new CompileSourceMessage('Test/test1.yml'),
+            ],
+            'all but one sources compiled' => [
+                'setup' => (new EnvironmentSetup())
+                    ->withSourceSetups([
+                        (new SourceSetup())->withPath('Test/test1.yml'),
+                        (new SourceSetup())->withPath('Test/test2.yml'),
+                    ])
+                    ->withTestSetups([
+                        (new TestSetup())->withSource('Test/test1.yml'),
+                    ]),
+                'expectedQueuedMessage' => new CompileSourceMessage('Test/test2.yml'),
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider subscribesToEventsDataProvider
+     *
+     * @param object[] $expectedQueuedMessages
+     */
+    public function testSubscribesToEvents(Event $event, array $expectedQueuedMessages): void
+    {
+        $environmentSetup = (new EnvironmentSetup())
+            ->withSourceSetups([
+                (new SourceSetup())->withPath('Test/test1.yml'),
+                (new SourceSetup())->withPath('Test/test2.yml'),
+            ])
+        ;
+
+        $this->environmentFactory->create($environmentSetup);
+
+        $this->messengerAsserter->assertQueueIsEmpty();
+
+        $this->eventDispatcher->dispatch($event);
+
+        $this->messengerAsserter->assertQueueCount(count($expectedQueuedMessages));
+        foreach ($expectedQueuedMessages as $messageIndex => $expectedQueuedMessage) {
+            $this->messengerAsserter->assertMessageAtPositionEquals($messageIndex, $expectedQueuedMessage);
+        }
+    }
+
+    /**
+     * @return array[]
+     */
+    public function subscribesToEventsDataProvider(): array
+    {
+        return [
+            PassedEvent::class => [
+                'event' => new PassedEvent(
+                    '/app/source/Test/test1.yml',
+                    (new MockSuiteManifest())
+                        ->withGetTestManifestsCall([])
+                        ->getMock()
+                ),
+                'expectedQueuedMessages' => [
+                    new CompileSourceMessage('Test/test1.yml'),
+                ],
+            ],
+            JobReadyEvent::class => [
+                'event' => new JobReadyEvent(),
+                'expectedQueuedMessages' => [
+                    new CompileSourceMessage('Test/test1.yml'),
+                    new TimeoutCheckMessage(),
+                ],
+            ],
+        ];
+    }
+}
