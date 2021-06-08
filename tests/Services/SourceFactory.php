5153293b255b3b5964c889f58868f0d9f90efe4d
Jon Cram <webignition@gmail.com>
2021-06-08 16:48:18 +0100

    Import sync end-to-end tests (#957)

---
+++ tests/Services/SourceFactory.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Services;
+
+use webignition\BasilWorker\PersistenceBundle\Entity\Source;
+use webignition\BasilWorker\PersistenceBundle\Services\Factory\SourceFactory as BundleSourceFactory;
+
+class SourceFactory
+{
+    public function __construct(
+        private BasilFixtureHandler $basilFixtureHandler,
+        private BundleSourceFactory $bundleSourceFactory,
+    ) {
+    }
+
+    /**
+     * @return array<string, Source>
+     */
+    public function createFromManifestPath(string $manifestPath): array
+    {
+        $manifestContent = (string) file_get_contents($manifestPath);
+        $sourcePaths = array_filter(explode("\n", $manifestContent));
+
+        $this->basilFixtureHandler->createUploadFileCollection($sourcePaths);
+
+        $sources = [];
+        foreach ($sourcePaths as $sourcePath) {
+            $sourceType = 0 === substr_count($sourcePath, 'Test/')
+                ? Source::TYPE_RESOURCE
+                : Source::TYPE_TEST;
+
+            $sources[$sourcePath] = $this->bundleSourceFactory->create($sourceType, $sourcePath);
+        }
+
+        return $sources;
+    }
+}
