1eb5280adb085f729024b96ab4c1c4bee73556de
Jon Cram <webignition@gmail.com>
2021-06-09 10:48:17 +0100

    Tidy up

--- tests/Integration/AbstractBaseIntegrationTest.php
+++ tests/Integration/AbstractBaseIntegrationTest.php
@@ -6,22 +6,14 @@ namespace App\Tests\Integration;
 
 use App\Tests\AbstractBaseFunctionalTest;
 use App\Tests\Services\EntityRemover;
-use Doctrine\ORM\EntityManagerInterface;
 
 abstract class AbstractBaseIntegrationTest extends AbstractBaseFunctionalTest
 {
-    protected EntityManagerInterface $entityManager;
     protected EntityRemover $entityRemover;
 
     protected function setUp(): void
     {
         parent::setUp();
-
-        $entityManager = self::$container->get(EntityManagerInterface::class);
-        self::assertInstanceOf(EntityManagerInterface::class, $entityManager);
-        if ($entityManager instanceof EntityManagerInterface) {
-            $this->entityManager = $entityManager;
-        }
 
         $entityRemover = self::$container->get(EntityRemover::class);
         \assert($entityRemover instanceof EntityRemover);
