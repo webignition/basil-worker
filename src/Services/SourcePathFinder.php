f6e9dd3369ccfbef131b2bceb8503a63f8c8aa5e
Jon Cram <webignition@gmail.com>
2021-06-18 15:37:06 +0100

    Import SourcePathFinder

---
+++ src/Services/SourcePathFinder.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services;
+
+use webignition\BasilWorker\PersistenceBundle\Entity\Source;
+use webignition\BasilWorker\PersistenceBundle\Services\Repository\TestRepository;
+use webignition\BasilWorker\PersistenceBundle\Services\Store\SourceStore;
+use webignition\StringPrefixRemover\DefinedStringPrefixRemover;
+
+class SourcePathFinder
+{
+    private DefinedStringPrefixRemover $compilerSourcePathPrefixRemover;
+
+    public function __construct(
+        private TestRepository $testRepository,
+        private SourceStore $sourceStore
+    ) {
+    }
+
+    public function setCompilerSourcePathPrefixRemover(
+        DefinedStringPrefixRemover $compilerSourcePathPrefixRemover
+    ): void {
+        $this->compilerSourcePathPrefixRemover = $compilerSourcePathPrefixRemover;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function findCompiledPaths(): array
+    {
+        $sources = $this->testRepository->findAllSources();
+
+        return $this->removeCompilerSourceDirectoryPrefixFromPaths($sources);
+    }
+
+    public function findNextNonCompiledPath(): ?string
+    {
+        $sourcePaths = $this->sourceStore->findAllPaths(Source::TYPE_TEST);
+        $testPaths = $this->testRepository->findAllSources();
+        $testPaths = $this->removeCompilerSourceDirectoryPrefixFromPaths($testPaths);
+
+        foreach ($sourcePaths as $sourcePath) {
+            if (!in_array($sourcePath, $testPaths)) {
+                return $sourcePath;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * @param string[] $paths
+     *
+     * @return string[]
+     */
+    private function removeCompilerSourceDirectoryPrefixFromPaths(array $paths): array
+    {
+        $strippedPaths = [];
+
+        foreach ($paths as $path) {
+            if (is_string($path)) {
+                $strippedPaths[] = $this->compilerSourcePathPrefixRemover->remove($path);
+            }
+        }
+
+        return $strippedPaths;
+    }
+}
---
+++ tests/Functional/Services/SourcePathFinderTest.php
@@ -0,0 +1,195 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Functional\Services;
+
+use App\Services\SourcePathFinder;
+use App\Tests\AbstractBaseFunctionalTest;
+use App\Tests\Model\EnvironmentSetup;
+use App\Tests\Model\JobSetup;
+use App\Tests\Model\SourceSetup;
+use App\Tests\Model\TestSetup;
+use App\Tests\Services\EnvironmentFactory;
+use webignition\BasilWorker\PersistenceBundle\Entity\Source;
+
+class SourcePathFinderTest extends AbstractBaseFunctionalTest
+{
+    private SourcePathFinder $sourcePathFinder;
+    private EnvironmentFactory $environmentFactory;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $sourcePathFinder = self::$container->get(SourcePathFinder::class);
+        if ($sourcePathFinder instanceof SourcePathFinder) {
+            $this->sourcePathFinder = $sourcePathFinder;
+        }
+
+        $environmentFactory = self::$container->get(EnvironmentFactory::class);
+        \assert($environmentFactory instanceof EnvironmentFactory);
+        $this->environmentFactory = $environmentFactory;
+    }
+
+    /**
+     * @dataProvider findNextNonCompiledPathDataProvider
+     */
+    public function testFindNextNonCompiledPath(
+        EnvironmentSetup $setup,
+        ?string $expectedNextNonCompiledSource
+    ): void {
+        $this->environmentFactory->create($setup);
+
+        self::assertSame($expectedNextNonCompiledSource, $this->sourcePathFinder->findNextNonCompiledPath());
+    }
+
+    /**
+     * @return array<mixed>
+     */
+    public function findNextNonCompiledPathDataProvider(): array
+    {
+        $sourceSetups = [
+            (new SourceSetup())
+                ->withType(Source::TYPE_RESOURCE)
+                ->withPath('Page/page1.yml'),
+            (new SourceSetup())
+                ->withType(Source::TYPE_TEST)
+                ->withPath('Test/test1.yml'),
+            (new SourceSetup())
+                ->withType(Source::TYPE_TEST)
+                ->withPath('Test/test2.yml'),
+            (new SourceSetup())
+                ->withType(Source::TYPE_RESOURCE)
+                ->withPath('Page/page2.yml'),
+        ];
+
+        return [
+            'no job' => [
+                'setup' => new EnvironmentSetup(),
+                'expectedNextNonCompiledSource' => null,
+            ],
+            'has job, no sources' => [
+                'setup' => (new EnvironmentSetup())
+                    ->withJobSetup(new JobSetup()),
+                'expectedNextNonCompiledSource' => null,
+            ],
+            'has job, has resource-only sources, no tests' => [
+                'setup' => (new EnvironmentSetup())
+                    ->withJobSetup(new JobSetup())
+                    ->withSourceSetups([
+                        $sourceSetups[0],
+                        $sourceSetups[3],
+                    ]),
+                'expectedNextNonCompiledSource' => null,
+            ],
+            'has job, has sources, no tests' => [
+                'setup' => (new EnvironmentSetup())
+                    ->withJobSetup(new JobSetup())
+                    ->withSourceSetups($sourceSetups),
+                'expectedNextNonCompiledSource' => 'Test/test1.yml',
+            ],
+            'test exists for first test source' => [
+                'setup' => (new EnvironmentSetup())
+                    ->withJobSetup(new JobSetup())
+                    ->withSourceSetups($sourceSetups)
+                    ->withTestSetups([
+                        (new TestSetup())
+                            ->withSource('{{ compiler_source_directory }}/Test/test1.yml'),
+                    ]),
+                'expectedNextNonCompiledSource' => 'Test/test2.yml',
+            ],
+            'test exists for all sources' => [
+                'setup' => (new EnvironmentSetup())
+                    ->withJobSetup(new JobSetup())
+                    ->withSourceSetups($sourceSetups)
+                    ->withTestSetups([
+                        (new TestSetup())
+                            ->withSource('{{ compiler_source_directory }}/Test/test1.yml'),
+                        (new TestSetup())
+                            ->withSource('{{ compiler_source_directory }}/Test/test2.yml'),
+                    ]),
+                'expectedNextNonCompiledSource' => null,
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider findCompiledPathsDataProvider
+     *
+     * @param string[] $expectedCompiledSources
+     */
+    public function testFindCompiledPaths(
+        EnvironmentSetup $setup,
+        array $expectedCompiledSources
+    ): void {
+        $this->environmentFactory->create($setup);
+
+        self::assertSame($expectedCompiledSources, $this->sourcePathFinder->findCompiledPaths());
+    }
+
+    /**
+     * @return array<mixed>
+     */
+    public function findCompiledPathsDataProvider(): array
+    {
+        $sources = [
+            'Test/testZebra.yml',
+            'Test/testApple.yml',
+        ];
+
+        $sourceSetups = [
+            (new SourceSetup())
+                ->withType(Source::TYPE_TEST)
+                ->withPath($sources[0]),
+            (new SourceSetup())
+                ->withType(Source::TYPE_TEST)
+                ->withPath($sources[1]),
+        ];
+
+        return [
+            'no job' => [
+                'setup' => new EnvironmentSetup(),
+                'expectedCompiledSources' => [],
+            ],
+            'has job, no sources' => [
+                'setup' => (new EnvironmentSetup())
+                    ->withJobSetup(new JobSetup()),
+                'expectedCompiledSources' => [],
+            ],
+            'has job, has sources, no tests' => [
+                'setup' => (new EnvironmentSetup())
+                    ->withJobSetup(new JobSetup())
+                    ->withSourceSetups($sourceSetups),
+                'expectedCompiledSources' => [],
+            ],
+            'test exists for first source' => [
+                'setup' => (new EnvironmentSetup())
+                    ->withJobSetup(new JobSetup())
+                    ->withSourceSetups($sourceSetups)
+                    ->withTestSetups([
+                        (new TestSetup())
+                            ->withSource('{{ compiler_source_directory }}/' . $sources[0]),
+                    ]),
+                'expectedCompiledSources' => [
+                    'Test/testZebra.yml',
+                ],
+            ],
+            'test exists for all sources' => [
+                'setup' => (new EnvironmentSetup())
+                    ->withJobSetup(new JobSetup())
+                    ->withSourceSetups($sourceSetups)
+                    ->withTestSetups([
+                        (new TestSetup())
+                            ->withSource('{{ compiler_source_directory }}/' . $sources[0]),
+                        (new TestSetup())
+                            ->withSource('{{ compiler_source_directory }}/' . $sources[1]),
+                    ]),
+                'expectedCompiledSources' => [
+                    $sources[0],
+                    $sources[1],
+                ],
+            ],
+        ];
+    }
+}
