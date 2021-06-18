23485b9d345315f50968afede520c53eeee6afa6
Jon Cram <webignition@gmail.com>
2021-06-18 17:55:59 +0100

    Import SourceRepository

---
+++ src/Repository/SourceRepository.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Repository;
+
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use webignition\BasilWorker\PersistenceBundle\Entity\Source;
+
+/**
+ * @method Source|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Source|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Source[]    findAll()
+ * @method Source[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ *
+ * @extends ServiceEntityRepository<Source>
+ */
+class SourceRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Source::class);
+    }
+}
---
+++ tests/Functional/Repository/SourceRepositoryTest.php
@@ -0,0 +1,118 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Functional\Repository;
+
+use App\Repository\SourceRepository;
+use webignition\BasilWorker\PersistenceBundle\Entity\EntityInterface;
+use webignition\BasilWorker\PersistenceBundle\Entity\Source;
+
+/**
+ * @extends AbstractEntityRepositoryTest<Source>
+ */
+class SourceRepositoryTest extends AbstractEntityRepositoryTest
+{
+    public function findOneByDataProvider(): array
+    {
+        return [
+            'type test' => [
+                'criteria' => [
+                    'type' => Source::TYPE_TEST,
+                ],
+                'orderBy' => null,
+                'expectedEntityIndex' => 0,
+            ],
+            'type resource' => [
+                'criteria' => [
+                    'type' => Source::TYPE_RESOURCE,
+                ],
+                'orderBy' => null,
+                'expectedEntityIndex' => 2,
+            ],
+            'path Test/test2.yml' => [
+                'criteria' => [
+                    'path' => 'Test/test2.yml',
+                ],
+                'orderBy' => null,
+                'expectedEntityIndex' => 1,
+            ],
+            'type test and path Test/test2.yml' => [
+                'criteria' => [
+                    'type' => Source::TYPE_TEST,
+                    'path' => 'Test/test2.yml',
+                ],
+                'orderBy' => null,
+                'expectedEntityIndex' => 1,
+            ],
+            'invalid type' => [
+                'criteria' => [
+                    'type' => 'Invalid',
+                ],
+                'orderBy' => null,
+                'expectedEntityIndex' => null,
+            ],
+        ];
+    }
+
+    public function countDataProvider(): array
+    {
+        return [
+            'type test' => [
+                'criteria' => [
+                    'type' => Source::TYPE_TEST,
+                ],
+                'expectedCount' => 2,
+            ],
+            'type resource' => [
+                'criteria' => [
+                    'type' => Source::TYPE_RESOURCE,
+                ],
+                'expectedCount' => 1,
+            ],
+            'path Test/test2.yml' => [
+                'criteria' => [
+                    'path' => 'Test/test2.yml',
+                ],
+                'expectedCount' => 1,
+            ],
+            'type test and path Test/test2.yml' => [
+                'criteria' => [
+                    'type' => Source::TYPE_TEST,
+                    'path' => 'Test/test2.yml',
+                ],
+                'expectedCount' => 1,
+            ],
+            'invalid type' => [
+                'criteria' => [
+                    'type' => 'Invalid',
+                ],
+                'expectedCount' => 0,
+            ],
+        ];
+    }
+
+    protected function getRepository(): ?SourceRepository
+    {
+        $repository = self::$container->get(SourceRepository::class);
+        if ($repository instanceof SourceRepository) {
+            return $repository;
+        }
+
+        return null;
+    }
+
+    protected function createSingleEntity(): EntityInterface
+    {
+        return Source::create(Source::TYPE_TEST, 'Test/test.yml');
+    }
+
+    protected function createEntityCollection(): array
+    {
+        return [
+            Source::create(Source::TYPE_TEST, 'Test/test1.yml'),
+            Source::create(Source::TYPE_TEST, 'Test/test2.yml'),
+            Source::create(Source::TYPE_RESOURCE, 'Page/page.yml'),
+        ];
+    }
+}
