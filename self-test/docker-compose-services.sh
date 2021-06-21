f794c94761d5a29da7d953136ced0e40ea7576ad
Jon Cram <webignition@gmail.com>
2021-06-21 12:01:51 +0100

    Output logs of container if self-test container test fails

--- self-test/docker-compose-services.sh
+++ self-test/docker-compose-services.sh
@@ -16,6 +16,7 @@ for Service in ${Services[*]}
     if ! sudo docker-compose --env-file .docker-compose.env ps --services --filter "status=running" | grep "$Service"; then
         echo "$Service not ok"
         docker-compose --env-file .docker-compose.env ps
+        docker-compose --env-file .docker-compose.env logs "$Service"
         exit 1
     fi
   done
