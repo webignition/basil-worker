45c1acc675fbe74913797769df4ea54c2048d38c
Jon Cram <webignition@gmail.com>
2021-06-22 12:06:02 +0100

    Remove use of webignition/symfony-messenger-json-message-serializer-bundle

--- src/Message/JobCompletedCheckMessage.php
+++ src/Message/JobCompletedCheckMessage.php
@@ -4,24 +4,6 @@ declare(strict_types=1);
 
 namespace App\Message;
 
-use webignition\JsonMessageSerializerBundle\Message\AbstractSerializableMessage;
-
-class JobCompletedCheckMessage extends AbstractSerializableMessage
+class JobCompletedCheckMessage
 {
-    public const TYPE = 'job-complete-check';
-
-    public static function createFromArray(array $data): self
-    {
-        return new JobCompletedCheckMessage();
-    }
-
-    public function getType(): string
-    {
-        return self::TYPE;
-    }
-
-    public function getPayload(): array
-    {
-        return [];
-    }
 }
--- src/Message/SendCallbackMessage.php
+++ src/Message/SendCallbackMessage.php
@@ -4,43 +4,19 @@ declare(strict_types=1);
 
 namespace App\Message;
 
-use webignition\JsonMessageSerializerBundle\Message\AbstractSerializableMessage;
 use webignition\SymfonyMessengerMessageDispatcher\Message\RetryableMessageInterface;
 
-class SendCallbackMessage extends AbstractSerializableMessage implements RetryableMessageInterface
+class SendCallbackMessage implements RetryableMessageInterface
 {
-    public const TYPE = 'send-callback';
-    public const PAYLOAD_KEY_CALLBACK_ID = 'callback_id';
-
     public function __construct(
         private int $callbackId,
         private int $retryCount = 0,
     ) {
     }
 
-    /**
-     * @param array<mixed> $data
-     */
-    public static function createFromArray(array $data): self
-    {
-        return new SendCallbackMessage((int) ($data[self::PAYLOAD_KEY_CALLBACK_ID] ?? 0));
-    }
-
     public function getCallbackId(): int
     {
         return $this->callbackId;
-    }
-
-    public function getType(): string
-    {
-        return self::TYPE;
-    }
-
-    public function getPayload(): array
-    {
-        return [
-            self::PAYLOAD_KEY_CALLBACK_ID => $this->callbackId,
-        ];
     }
 
     public function getRetryCount(): int
